a1536f90598f2dc0bfd9649d4fa7b4026f5dbba8
env. <162432464+belowzeroiq@users.noreply.github.com>
2026-01-16 19:30:05 +0000

    push

---
+++ app/src/main/java/com/belowzeroiq/ksunotify/BootReceiver.java
@@ -0,0 +1,21 @@
+package com.belowzeroiq.ksunotify;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+
+public class BootReceiver extends BroadcastReceiver {
+
+    @Override
+        public void onReceive(Context context, Intent intent) {
+                if (Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())) {
+                            Intent svc = new Intent(context, NotifyService.class);
+                                        if (Build.VERSION.SDK_INT >= 26) {
+                                                        context.startForegroundService(svc);
+                                                                    } else {
+                                                                                    context.startService(svc);
+                                                                                                }
+                                                                                                        }
+                                                                                                            }
+                                                                                                            }
